fdb6d6b854e0190e55b84f87b02eee668fe3b2d0
ColderNegative <lewis.x.quinn@gmail.com>
2021-09-09 13:20:09 +0200

    working on parsing vertices and normals

--- Notes.md
+++ Notes.md
@@ -1,6 +1,11 @@
 ## Object file importing
-currently very convoluted 
-add automation to calculating number of faces and verticies
-loop through each line and check for if there is a solo 'v' or 'f' character
-have a method that pulls vertex data
-hava method that pulls face data
+- currently very convoluted 
+- add automation to calculating number of faces and verticies
+- loop through each line and check for if there is a solo 'v' or 'f' character
+- have a method that pulls vertex data
+- hava method that pulls face data
+- join 2d arrays into 3d array which can be pulled into the vector objects file
+- in the vector objects file the data will put into object arrays 
+- the object arrays will then be put through the matrix file where rotation and projection will be solved 
+- the data will then be converted to an array of 2d points 
+- another array will indicate the connections for the faces which are triangles 
--- ObjImport.java
+++ ObjImport.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -16,49 +15,84 @@ public class ObjImport {
             File myObj = new File(filename);
             Scanner myReader = new Scanner(myObj);
             double[][] objVerticies = new double[vert][3];
-            double[][] objFaces = new double[faces][3];
+            String[][] objFaces = new String[faces][3];
             int j = 0;
+            int u = 0;
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                System.out.println(data);
                 if (data.length() > 0) {
+                    
                     if (data.charAt(0) == 'v') {
                         
                         String coord = "";
-                        int u = 0;
+                        u = 0;
                         if (data.charAt(2) == ' ') {
-                            break;
+                            continue;
                         } else {
+                            System.out.println(u + " " + j);
                             for (int i = 2; i < data.length(); i++) {
                                 
                                 if (data.charAt(i) != ' ') {
                                     if (i == data.length()-1) {
                                         coord += data.charAt(i);
                                         objVerticies[j][u] = Double.parseDouble(coord);
+                                        coord = "";
+                                    } else {
+                                        coord += data.charAt(i);
+                                    }
+                                    
+                                } else {
+                                    objVerticies[j][u] = Double.parseDouble(coord);
+                                    coord = "";
+                                    u++;
+                                }
+                            }
+                        
+                        }
+                        
+                    } else if (data.charAt(0) == 'f') {
+                        System.out.println(data);
+                        
+                        String coord = "";
+                        u = 0;
+                        
+                        
+                        if (data.charAt(2) == ' ') {
+                            continue;
+                        } else {
+                            System.out.println(u + " " + j);
+                            for (int i = 2; i < data.length(); i++) {
+                                
+                                if (data.charAt(i) != ' ') {
+                                    if (i == data.length()-1) {
+                                        coord += data.charAt(i);
+                                        objFaces[j][u] = coord;
                                         System.out.println(coord);
                                         coord = "";
                                     } else {
                                         coord += data.charAt(i);
                                     }
                                     
-                                } else if (i == data.length()-1) {
-                                    
                                 } else {
-                                    objVerticies[j][u] = Double.parseDouble(coord);
+                                    objFaces[j][u] = coord;
                                     System.out.println(coord);
                                     coord = "";
                                     u++;
                                 }
+                            
                             }
                             
                         }
-                        j++;
+                        
+                        
                     }
+                j++;
                 }
-                
             }
             myReader.close();
             System.out.println(Arrays.deepToString(objVerticies));
+            System.out.println(Arrays.deepToString(objFaces));
+
             } catch (FileNotFoundException e) {
             System.out.println("An error occurred with file reading.");
             e.printStackTrace();
